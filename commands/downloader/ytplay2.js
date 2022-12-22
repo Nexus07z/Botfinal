c56c6b3a0b734f7022c97ef258587514ee65cc31
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-21 23:56:09 -0500

    Add files via upload

---
+++ commands/downloader/ytplay2.js
@@ -0,0 +1,36 @@
+const { fetchUrl } = require("../../lib/Function")
+const yts = require('yt-search')
+module.exports = {
+    name: "ytplay2",
+    alias: ["play2"],
+    use: "<query>",
+    desc: "Download Media From https://youtube.com",
+    type: "downloader",
+    example: "%prefix%command <url>",
+    start: async(killua, m, { text }) => {
+        let search = await yts(text)
+        let segmento = search.videos[Math.floor(Math.random() * search.videos.length)]
+        
+        let buttons = [
+            {buttonId: `${prefix}ytmp3 https://youtu.be/${segmento.videoId}`, buttonText: {displayText: '♫ Audio'}, type: 1},
+            {buttonId: `${prefix}ytmp4 https://youtu.be/${segmento.videoId}`, buttonText: {displayText: '► Video'}, type: 1}
+        ]
+
+        let caption = `*Youtube Play*\n\n`
+        caption += `*Titulo:* ${segmento.title}\n`
+        caption += `*ID Youtube:* ${segmento.videoId}\n`
+        caption += `*Duración:* ${segmento.timestamp}\n`
+        caption += `*Link:* https://youtu.be/${segmento.videoId}\n`
+
+        let buttonMessage = {
+            image: { url: segmento.thumbnail },
+            caption: caption,
+            footer: config.footer,
+            buttons: buttons,
+            headerType: 4
+        }
+
+        killua.sendMessage(m.from, buttonMessage, { quoted: m })
+    },
+    isQuery: true
+}
