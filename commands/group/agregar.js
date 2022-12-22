58be49d6c1ed5f85ea20ba7a7b16bed781924020
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-22 18:50:39 -0500

    Add files via upload

--- commands/group/agregar.js
+++ commands/group/agregar.js
@@ -6,8 +6,8 @@ module.exports = {
 	type: "group",
 	start: async(killua, m, { text, prefix, command }) => {
         if (!text) return m.reply(`Example: ${prefix + command} @tag`)
-		let users = m.quoted ? [m.quoted.sender] : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
-		for (let i of users) await killua.groupParticipantsUpdate(m.from, [i], "add")
+		let me = m.quoted ? [m.quoted.sender] : text.replace(/[^0-9]/g, '')+'@s.whatsapp.net'
+		await killua.groupParticipantsUpdate(m.from, [me], "add")
 
 		await m.reply("Suksess")
 	},
