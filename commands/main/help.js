06ae3f2d30b2d21f769a159aa263f2483b4e75e7
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-21 22:31:04 -0500

    Add files via upload

--- commands/main/help.js
+++ commands/main/help.js
@@ -10,24 +10,24 @@ module.exports = {
             let name = args[0].toLowerCase()
             let cmd = commands.get(name) || Array.from(commands.values()).find((v) => v.alias.includes(name))
             if (!cmd || cmd.type == "hide") return m.reply("No Command Found")
-            else data.push(`*Command :* ${cmd.name.replace(/^\w/, c => c.toUpperCase())}`)
-            if (cmd.alias) data.push(`*Alias :* ${cmd.alias.join(", ")}`)
-            if (cmd.use) data.push(`*Use:* ${cmd.use}`);
-            if (cmd.desc) data.push(`*Description :* ${cmd.desc}\n`)
-            if (cmd.example) data.push(`*Example :* ${cmd.example.replace(/%prefix/gi, prefix).replace(/%command/gi, cmd.name).replace(/%text/gi, text)}`)
-            return m.reply(`*Info Command ${cmd.name.replace(/^\w/, c => c.toUpperCase())}*\n\n${data.join("\n")}`)
+            else data.push(`*Información de * ${cmd.name.replace(/^\w/, c => c.toUpperCase())}`)
+            if (cmd.alias) data.push(`*Alias:* ${cmd.alias.join(", ")}`)
+            if (cmd.use) data.push(`*Uso:* ${cmd.use}`);
+            if (cmd.desc) data.push(`*Descripción :* ${cmd.desc}\n`)
+            if (cmd.example) data.push(`*Ejemplo :* ${cmd.example.replace(/%prefix/gi, prefix).replace(/%command/gi, cmd.name).replace(/%text/gi, text)}`)
+            return m.reply(`*Comando: ${prefix}${cmd.name.replace(/^\w/, c => c.toLowerCase())}*\n\n${data.join("\n")}`)
         } else {
-            let teks = `Hello, ${pushName === undefined ? sender.split("@")[0] : pushName}\nHere is the Command List\n\n`
+            let teks = `Hola, ${pushName === undefined ? sender.split("@")[0] : pushName}\nAquí está la lista de comandos\n\n`
 
             for (let type of commands.type) {
-                teks += `┌──⭓ *${toUpper(type)} Menu*\n`
+                teks += `┌──⭓ *Menú de ${toUpper(type)}*\n`
                 teks += `│\n`
                 teks += `${commands.list[type].filter(v => v.type !== "hide").map((cmd) => `│⭔ ${prefix + cmd.name} ${cmd.use ? " " + cmd.use : ""}`).join("\n")}\n`
                 teks += `│\n`
                 teks += `└───────⭓\n\n`
             }
 
-            teks += `Send ${prefix}help followed by a command name to get detail of command, ex: ${prefix}help sticker`;
+            teks += `Escribe el comando *${prefix}?* seguido del nombre de un comando para obtener información respecto a su uso.\n\n*Por ejemplo:*\n\n *${prefix}? sticker*`;
             
             let templateButtons = [
                 {index: 1, urlButton: { displayText: "Source Code", url: "https://github.com/zhwzein/Killua-Zoldyck" }},
