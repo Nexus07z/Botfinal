5e10b5a25ab4736b07ed3414e4cbce8e61724ae0
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-21 22:36:33 -0500

    Add files via upload

--- commands/main/help.js
+++ commands/main/help.js
@@ -10,14 +10,14 @@ module.exports = {
             let name = args[0].toLowerCase()
             let cmd = commands.get(name) || Array.from(commands.values()).find((v) => v.alias.includes(name))
             if (!cmd || cmd.type == "hide") return m.reply("No Command Found")
-            else data.push(`*Información de * ${cmd.name.replace(/^\w/, c => c.toUpperCase())}`)
+            else data.push(`*Comando:* ${prefix}${cmd.name.replace(/^\w/, c => c.toUpperCase())}`)
             if (cmd.alias) data.push(`*Alias:* ${cmd.alias.join(", ")}`)
             if (cmd.use) data.push(`*Uso:* ${cmd.use}`);
             if (cmd.desc) data.push(`*Descripción :* ${cmd.desc}\n`)
             if (cmd.example) data.push(`*Ejemplo :* ${cmd.example.replace(/%prefix/gi, prefix).replace(/%command/gi, cmd.name).replace(/%text/gi, text)}`)
-            return m.reply(`*Comando: ${prefix}${cmd.name.replace(/^\w/, c => c.toLowerCase())}*\n\n${data.join("\n")}`)
+            return m.reply(`*Información de ${cmd.name.replace(/^\w/, c => c.toLowerCase())}*\n\n${data.join("\n")}`)
         } else {
-            let teks = `Hola, ${pushName === undefined ? sender.split("@")[0] : pushName}\nAquí está la lista de comandos\n\n`
+            let teks = `Hola, *${pushName === undefined ? sender.split("@")[0] : pushName}*\n\nAquí está la lista de comandos.\n\n`
 
             for (let type of commands.type) {
                 teks += `┌──⭓ *Menú de ${toUpper(type)}*\n`
