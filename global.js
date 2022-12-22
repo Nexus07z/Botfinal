cfdd013124dbf32f98620db716bf4d89c5419e96
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-21 19:46:46 -0500

    Add files via upload

--- global.js
+++ global.js
@@ -4,21 +4,21 @@ global.user = require("./data/user")
 global.group = require("./data/group")
 global._user = JSON.parse(fs.readFileSync("./database/user.json"))
 global._group = JSON.parse(fs.readFileSync("./database/group.json"))
-global.prefix = [',']
+global.prefix = ['.']
 
 global.mess = (type, m) => {
     let msg = {
-        wait: 'Wait, in progress',
-        owner: 'Perintah ini hanya dapat digunakan oleh Owner!',
-        premium: 'Perintah ini hanya dapat digunakan oleh Premium!',
-        group: 'Perintah ini hanya dapat digunakan di group!',
-        private: 'Perintah ini hanya dapat digunakan di private chat!',
-        admin: 'Perintah ini hanya dapat digunakan oleh admin group!',
-        botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut',
-        bot: 'Fitur ini hanya dapat diakses oleh Bot',
-        dead: 'Fitur ini sedang dimatikan!',
-        media: 'Reply media',
-        error: "No Results Found"
+        wait: '*Espera un momento por favor...*',
+        owner: '[❗] *ALERTA*\n\n*¡Este comando solo puede ser utilizado por el propietario!*',
+        premium: '[❗] *ALERTA*\n\n*¡Este comando solo puede ser utilizado por usuarios Premium!*',
+        group: '[❗] *ALERTA*\n\n*¡Este comando solo puede ser utilizado en grupos!*',
+        private: '[❗] *ALERTA*\n\n*¡Este comando solo se puede usar en el chat privado!*',
+        admin: '[❗] *ALERTA*\n\n*¡Este comando solo puede ser utilizado por los administradores del grupo!*',
+        botAdmin: '[❗] *ALERTA*\n\n*¡Este comando solo se puede usar cuando el Bot es administrador!*',
+        bot: '*Este comando es para el uso exclusivo del Bot.*',
+        dead: '*Este comando está actualmente deshabilitado.*',
+        media: '*Debes etiquetar un archivo multimedia con el comando.*',
+        error: "*Ocurrió un problema, puedes intentarlo nuevamente más tarde.*"
     }[type]
     if (msg) return m.reply(msg, m.from, { quoted: m })
 }
