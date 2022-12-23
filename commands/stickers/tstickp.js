03e6cf6bdd504efbf3af0cdcb0b0bc4442f0048a
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-23 12:10:01 -0500

    Add files via upload

--- commands/stickers/tstickp.js
+++ commands/stickers/tstickp.js
@@ -10,6 +10,7 @@ module.exports = {
         let respuestacomando = `Falta agregar texto con los colores dejando un espacio al lado del siguiente comando: *${prefix + command}*\n\n*Por ejemplo:*\n\n*${prefix + command} Hola mundo|white|black*`
         if (!text) return m.reply(`${respuestacomando}`)
         if (!text.includes('|')) return m.reply(`${respuestacomando}`)
+        
         try {
             
             let [a, b, c] = text.split`|`
