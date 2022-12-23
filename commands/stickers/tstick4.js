aaae3c5dfc6af93fd8b32a7c698849ebdc828e19
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-23 10:26:15 -0500

    Add files via upload

--- commands/stickers/tstick4.js
+++ commands/stickers/tstick4.js
@@ -5,17 +5,17 @@ module.exports = {
     desc: "Convierte texto en un sticker.",
     type: "Stickers",
     example: "\n*%prefix%command Hola mundo*\n\n*%prefix%command Puedo escribir lo que sea.*",
-    start: async(killua, m, { command, prefix, text }) => {
+    start: async(killua, m, { command, prefix, text }) => {  
         
         let respuestacomando = `Falta agregar texto dejando un espacio al lado del siguiente comando: *${prefix + command}*\n\n*Por ejemplo:*\n\n*${prefix + command} Hola mundo*`
         if (!text) return m.reply(`${respuestacomando}`)
 
         try {
 
-            await killua.sendFile(m.from, global.api("zenz", "/creator/ttp", { text: text }, "apikey"), "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
-
+            await killua.sendFile(m.from, `https://api.lolhuman.xyz/api/ttp4?apikey=${global.apilol}&text=${text}`,  "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+        
         } catch (e) {
             m.reply(`*Ocurrió un problema, puedes intentarlo nuevamente más tarde.*`)
-        }
-    }   
+        }   
+    }
 }
