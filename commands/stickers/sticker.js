b01798072fe2301854763ce8a6a6f38118d4d547
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-23 01:41:59 -0500

    Add files via upload

---
+++ commands/stickers/sticker.js
@@ -0,0 +1,40 @@
+
+const { delay, extractMessageContent } = require("@adiwajshing/baileys")
+const { isUrl, fetchUrl } = require("../../lib/Function")
+
+module.exports = {
+    name: "sticker",
+    alias: ["s","sticker"],
+    use: "<Respuesta> <Comentario>",
+    desc: "Convierte imágenes, gif animados y videos a sticker.",
+    type: "Stickers",
+    example: "\nArchivo a sticker: %prefix%command <Archivo Multimedia Comentado>\nFoto de perfil: %prefix%command @tag\nLink: %prefix%command [Link del archivo]",
+    start: async(killua, m, { command, prefix, text, quoted, mime }) => {  
+        if (/image|video|sticker/.test(mime)) {
+            let download = await quoted.download()
+            killua.sendFile(m.from, download, "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+        } else if (quoted.mentions[0]) {
+            let url = await killua.profilePictureUrl(quoted.mentions[0], "image")
+            killua.sendFile(m.from, url, "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+        } else if (isUrl(text)) {
+            if (isUrl(text)) killua.sendFile(m.from, isUrl(text)[0], "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+            else m.reply('No Url Match')
+        } else if (text) {
+            let fetch = await fetchUrl(global.api("zenz", "/searching/stickersearch", { query: text }, "apikey"))
+            for (let url of fetch.result) {
+                await delay(1000)
+                killua.sendFile(m.from, url, "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+            }
+        } else if (quoted.type == "templateMessage") {
+            let message = quoted.imageMessage || quoted.videoMessage
+            let download = await killua.downloadMediaMessage(message)
+            killua.sendFile(m.from, download, "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+        } else if (quoted.type == "buttonsMessage") {
+            let message = quoted.imageMessage || quoted.videoMessage
+            let download = await killua.downloadMediaMessage(message)
+            killua.sendFile(m.from, download, "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+        } else {
+            return m.reply(`Debes responder o comentar un archivo multimedia con el comando: *${prefix + command}*`, m.from, { quoted: m })
+        }
+    }
+}
---
+++ commands/stickers/tstick.js
@@ -0,0 +1,26 @@
+const { delay, extractMessageContent } = require("@adiwajshing/baileys")
+const { isUrl, fetchUrl } = require("../../lib/Function")
+
+module.exports = {
+    name: "tstick",
+    alias: ["ttp"],
+    use: "[texto]",
+    desc: "Convierte texto en un sticker.",
+    type: "Stickers",
+    example: "\n*%prefix%command Hola mundo*\n\n*%prefix%command Puedo escribir lo que sea.*",
+    start: async(killua, m, { command, prefix, text, quoted, mime }) => {  
+        
+        let respuestacomando = `Falta agregar texto dejando un espacio al lado del siguiente comando: *${prefix + command}*\n\n*Por ejemplo:*\n\n*${prefix + command} Hola mundo*`
+        if (!text) return m.reply(`${respuestacomando}`)
+
+        try {
+
+            await killua.sendFile(m.from, `https://api.lolhuman.xyz/api/ttp?apikey=${global.apilol}&text=${text}`,  "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+        
+        } catch (e) {
+        m.reply(`*Ocurrió un problema, puedes intentarlo nuevamente más tarde.*`)
+        }
+        
+        
+    }
+}
