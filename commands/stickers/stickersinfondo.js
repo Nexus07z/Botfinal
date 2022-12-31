9c00edb8069a74646f0640034af13b649a5ef0ff
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2022-12-30 22:04:34 -0500

    Add files via upload

--- commands/stickers/stickersinfondo.js
+++ commands/stickers/stickersinfondo.js
@@ -12,7 +12,8 @@ module.exports = {
     start: async(killua, m, { command, prefix, quoted, mime, text }) => {
         if (/image/.test(mime)) {
             let download = await killua.downloadAndSaveMediaMessage(quoted)
-            file_name = getRandom('jpg')
+            file_name = getRandom('.png')
+            file_name2 = getRandom('.webp')
             request({
                 url: `https://api.lolhuman.xyz/api/removebg?apikey=4fda13ee5ed767eef2174d23`,
                 method: 'POST',
@@ -23,10 +24,21 @@ module.exports = {
             }, async function(error, response, body) {
                 fs.unlinkSync(download)
                 fs.writeFileSync(file_name, body, "binary")
-                ini_buff = fs.readFileSync(file_name)
-                await killua.sendFile(m.from, ini_buff, "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] }).then(() => {
-                    fs.unlinkSync(file_name)
-                })
+                ffmpeg(`./${file_name}`)
+                                .input(file_name)
+                                .on('error', function(err) {
+                                    console.log(err)
+                                    fs.unlinkSync(file_name)
+                                })
+                                .on('end', function() {
+                                    ini_buff = fs.readFileSync(file_name2)
+                                    killua.sendFile(m.from, ini_buff, "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
+                                    fs.unlinkSync(file_name2)
+                                })
+                                .addOutputOptions([`-vcodec`, `libwebp`, `-vf`, `scale='min(320,iw)':min'(320,ih)':force_original_aspect_ratio=decrease,fps=15, pad=320:320:-1:-1:color=white@0.0, split [a][b]; [a] palettegen=reserve_transparent=on:transparency_color=ffffff [p]; [b][p] paletteuse`])
+                                .toFormat('webp')
+                                .save(file_name2)
+                
             })
         } else if (isUrl(text)) {
             killua.sendFile(m.from, global.apilol("lol", "/removebg", { img: isUrl(text)[0] }, "apikey"), "", m, { asSticker: true, author: config.exif.author, packname: config.exif.packname, categories: ['😄','😊'] })
