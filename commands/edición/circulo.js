6ff90af02e5144c26b4eef67249437bf9e8e7b75
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2023-01-01 19:05:04 -0500

    Add files via upload

--- commands/edición/circulo.js
+++ commands/edición/circulo.js
@@ -10,25 +10,29 @@ module.exports = {
     type: "Edición de fotos",
     example: `\n*%prefix%command <Respuesta multimedia>*`,
     start: async(killua, m, { command, prefix, quoted, mime }) => {
-        if (!quoted) return  m.reply(`Reply to Supported media With Caption ${prefix + command}`)
+
         if (/image/.test(mime)) {
-            let download = await killua.downloadAndSaveMediaMessage(quoted)
-            file_name = getRandom('jpg')
-            request({
-                url: global.api("zenz", "/photoeditor/circle", {}, "apikey"),
-                method: 'POST',
-                formData: {
-                    "sampleFile": fs.createReadStream(download)
-                },
-                encoding: "binary"
-            }, async function(error, response, body) {
-                fs.unlinkSync(download)
-                fs.writeFileSync(file_name, body, "binary")
-                ini_buff = fs.readFileSync(file_name)
-                await killua.sendFile(m.from, ini_buff, "", m).then(() => {
-                    fs.unlinkSync(file_name)
+            try {
+                let download = await killua.downloadAndSaveMediaMessage(quoted)
+                file_name = getRandom('jpg')
+                request({
+                    url: global.api("zenz", "/photoeditor/circle", {}, "apikey"),
+                    method: 'POST',
+                    formData: {
+                        "sampleFile": fs.createReadStream(download)
+                    },
+                    encoding: "binary"
+                }, async function(error, response, body) {
+                    fs.unlinkSync(download)
+                    fs.writeFileSync(file_name, body, "binary")
+                    ini_buff = fs.readFileSync(file_name)
+                    await killua.sendFile(m.from, ini_buff, "", m).then(() => {
+                        fs.unlinkSync(file_name)
+                    })
                 })
-            });
+            } catch (e) {
+                m.reply(`*Ocurrió un problema, puedes intentarlo nuevamente más tarde.*`)
+            }
         } else {
             return m.reply(`Debes responder o comentar una imagen con el comando: *${prefix + command}*`, m.from, { quoted: m })
         }
