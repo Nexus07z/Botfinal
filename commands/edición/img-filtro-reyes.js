06567f673c835a87bd2c19a31c75e2ba827a0290
Nexus07z <76958607+Nexus07z@users.noreply.github.com>
2023-01-04 00:18:10 -0500

    Add files via upload

---
+++ commands/edición/img-filtro-reyes.js
@@ -0,0 +1,38 @@
+const { isUrl } = require("../../lib/Function")
+let { TelegraPh } = require('../../lib/Uploader')
+
+module.exports = {
+    name: "img-filtro-reyes",
+    alias: ["imgfreyes"],
+    use: "<Respuesta>",
+    desc: "Convierte una imagen con filtro reyes.",
+    type: "Edición de fotos",
+    example: `\n*%prefix%command <Respuesta multimedia>*\n\n*%prefix%command https://telegra.ph/file/d7628ed80228711f4a95b.jpg*`,
+    start: async(killua, m, { command, prefix, quoted, mime, text }) => {
+
+        if (/image/.test(mime)) {
+            try {
+                
+                let cargador = await killua.downloadAndSaveMediaMessage(quoted)
+                let link = await TelegraPh(cargador)
+                await killua.sendFile(m.from, global.apilol("lol", "/filter/reyes", { img: link }, "apikey"), "", m)
+
+            } catch (e) {
+                m.reply(`*Ocurrió un problema, puedes intentarlo nuevamente más tarde.*`)
+            }
+
+        } else if (isUrl(text)) {
+            try {
+
+                await killua.sendFile(m.from, global.apilol("lol", "/filter/reyes", { img: isUrl(text)[0] }, "apikey"), "", m)
+            
+            } catch (e) {
+                m.reply(`*Ocurrió un problema, puedes intentarlo nuevamente más tarde.*`)
+            }
+
+        }   else {
+                let respuestacomando = `Debes responder o comentar una imagen con el comando: *${prefix + command}*`
+                return m.reply(`${respuestacomando}`)
+        }
+    }
+}
